6aacfe8f8c5fcff696cdaa84f1a9a0c8516df0e0
Lucas <me@lhvy.dev>
2022-07-08 21:21:07 +1000

    Add fabric.mod.json

--- src/main/java/dev/lhvy/autorespawn/AutoRespawn.java
+++
@@ -1,9 +0,0 @@
-package dev.lhvy.autorespawn;
-
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
-
-public class AutoRespawn implements ModInitializer {
-	@Override
-	public void onInitialize(ModContainer mod) {}
-}
