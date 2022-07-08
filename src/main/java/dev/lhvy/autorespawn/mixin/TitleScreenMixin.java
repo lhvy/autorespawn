d25480165467ec5d451822e9a58964d4de7fdc10
Lucas <me@lhvy.dev>
2022-07-08 13:06:42 +1000

    Fill in template

--- src/main/java/dev/lhvy/autorespawn/mixin/TitleScreenMixin.java
+++ src/main/java/dev/lhvy/autorespawn/mixin/TitleScreenMixin.java
@@ -1,16 +1,17 @@
-package com.example.example_mod.mixin;
+package dev.lhvy.autorespawn.mixin;
 
-import com.example.example_mod.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import dev.lhvy.autorespawn.AutoRespawn;
+
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		AutoRespawn.LOGGER.info("This line is printed by an AutoRespawn mixin!");
 	}
 }
